902bcb4a454b8aa3015b7b88aa1bbaebac28757e
Massimiliano <massimiliano.giovagnoli@innoteam.it>
2020-01-22 10:29:10 +0100

    Update EKS cluster module's version in examples
    Update from 0.13.0 to 0.16.0.

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -42,7 +42,7 @@ module "subnets" {
 }
 
 module "eks_cluster" {
-  source                = "git::https://github.com/cloudposse/terraform-aws-eks-cluster.git?ref=tags/0.13.0"
+  source                = "git::https://github.com/cloudposse/terraform-aws-eks-cluster.git?ref=tags/0.16.0"
   namespace             = var.namespace
   stage                 = var.stage
   name                  = var.name
