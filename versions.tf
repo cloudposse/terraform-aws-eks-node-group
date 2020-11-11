028f549460638569bbf6b3c61f48835066f78701
Danilo Alves <danilo.alves@wavy.global>
2020-11-11 15:15:03 -0300

    Fix version AWS Provider

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 0.13.3"
 
   required_providers {
-    aws      = ">= 3.0"
+    aws      = ">= 2.0, < 4.0"
     template = ">= 2.0"
     local    = ">= 1.3"
     random   = ">= 2.0"
