f967f5a522eef36416b65686835d6181a6a390f7
Abhinav Khanna <abhinav.khanna@salesforce.com>
2021-03-15 11:25:26 -0500

    fix(aws_launch_template): fixed bridgecrew reported error

--- variables.tf
+++ variables.tf
@@ -292,7 +292,6 @@ variable "launch_template_metadata_options_http_endpoint" {
 }
 
 variable "launch_template_metadata_options_http_tokens" {
-  default     = "optional"
   type        = string
   description = "Whether or not the metadata service requires session tokens, also referred to as Instance Metadata Service Version 2 (IMDSv2). Can be optional or required"
 }
