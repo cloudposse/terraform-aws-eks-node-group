32d9c867b7e192b686f599f828de2b339387f0a2
Chris Vittoria <cvittoria@sonatype.com>
2021-07-26 15:07:25 -0400

    Add taint block to cbd ng. * Fix map syntax

--- main.tf
+++ main.tf
@@ -98,9 +98,9 @@ locals {
     # Configure taints
     taints = length(var.kubernetes_taints) > 0 ? { for k, v in var.kubernetes_taints : k => v } : {}
     taint_lookup_map = {
-      "NoSchedule"       = "NO_SCHEDULE"
-      "NoExecute"        = "NO_EXECUTE"
-      "PreferNoSchedule" = "PREFER_NO_SCHEDULE"
+      NoSchedule       = "NO_SCHEDULE"
+      NoExecute        = "NO_EXECUTE"
+      PreferNoSchedule = "PREFER_NO_SCHEDULE"
     }
   }
 }
@@ -267,6 +267,14 @@ resource "aws_eks_node_group" "cbd" {
     content {
       ec2_ssh_key               = local.ng.ec2_ssh_key
       source_security_group_ids = local.ng.source_security_group_ids
+    }
+  }
+
+  dynamic "taint" {
+    for_each = local.ng.taints
+    content {
+      key    = taint.key
+      effect = local.ng.taint_lookup_map[taint.value]
     }
   }
 
