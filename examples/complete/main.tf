4fa2508f5a8381b9107ec0262091dc1de1d105c4
aknysh <andriy.knysh@gmail.com>
2023-09-26 13:30:55 -0400

    Add `force_update_version` and `include_kubernetes_version_in_keepers` variables

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -122,7 +122,7 @@ module "eks_node_group" {
   source = "../../"
 
   subnet_ids         = module.this.enabled ? module.subnets.public_subnet_ids : ["filler_string_for_enabled_is_false"]
-  cluster_name       = local.enabled ? module.eks_cluster.eks_cluster_id : ""
+  cluster_name       = module.this.enabled ? module.eks_cluster.eks_cluster_id : "disabled"
   instance_types     = var.instance_types
   desired_size       = var.desired_size
   min_size           = var.min_size
