6962cfe389b9d4e849473620959fb80f67bc20cd
John C. Bland II <johncblandii@users.noreply.github.com>
2025-11-19 10:43:23 -0600

    feat: enable node auto-repair by default (#225)
    * feat: enable node auto-repair by default
    
    * chore: update variable description

--- variables.tf
+++ variables.tf
@@ -475,8 +475,8 @@ variable "node_group_terraform_timeouts" {
 
 variable "node_repair_enabled" {
   type        = bool
-  description = "The node auto-repair configuration for the node group will be enabled. Defaults to false"
-  default     = false
+  description = "The node auto-repair configuration for the node group will be enabled. Defaults to true"
+  default     = true
 }
 
 variable "detailed_monitoring_enabled" {
